dab79b9cbb834b80683325c4fa3ec7f10781f2c4
Francis Bulus <notchera@gmail.com>
2019-09-16 14:33:57 +0100

    imported movie component in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import MovieList from './Movies/MovieList'
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import {Route} from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -13,7 +15,7 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route path = "/" component = {MovieList}/>
-      <Route/>
+      <Route path="/movies/:id" component={Movie}/>
     </div>
   );
 };
