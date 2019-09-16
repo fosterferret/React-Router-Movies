51149fe84a4ff93729aff52ce8a744b35e8ac106
Francis Bulus <notchera@gmail.com>
2019-09-16 18:07:35 +0100

    imported NavLink in savedlist component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
+        .get("http://localhost:5000/api/movies")
         .then(response => {
           setMovies(response.data);
         })
@@ -21,16 +21,11 @@ const MovieList = props => {
 
   return (
     <div className="movie-list">
-      {movies.map(
-        movie => (
-          console.log(movie),
-          (
-            <Link to={`/movies/${movie.id}`}>
-              <MovieDetails key={movie.id} movie={movie} />
-            </Link>
-          )
-        )
-      )}
+      {movies.map(movie => (
+        <Link to = {`/movies/${movie.id}`} key={movie.id}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
+      ))}
     </div>
   );
 };
