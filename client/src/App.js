5bff555ac3dd1586d2d68c55c520a68f6a8414b3
Francis Bulus <notchera@gmail.com>
2019-09-16 20:57:01 +0100

    added functionality to save movie - stretch task

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 import MovieList from './Movies/MovieList'
 import SavedList from './Movies/SavedList';
 import Movie from './Movies/Movie';
-import {Route} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    const saved = savedList.find(el => el.title === movie.title)
+    if (!saved) {
+      setSavedList( [...savedList, movie] );
+    } 
   };
 
   return (
     <div>
-      <SavedList list={savedList} />
+      <NavLink to='/savedList'><SavedList list={savedList} /></NavLink>
       <Route exact path = "/" component = {MovieList}/>
-      <Route path="/movies/:id" component={Movie}/>
+      <Route path="/movies/:id" render={props => <Movie {...props} addToSavedList={addToSavedList} savedList={savedList} />}/>
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
 
-const Movie = ({match}) => {
+const Movie = (props) => {
   const [movie, setMovie] = useState();
 
   useEffect(() => {
-    const id = match.params.id;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,13 +18,13 @@ const Movie = ({match}) => {
       .catch(error => {
         console.error(error);
       });
-  }, [match.params.id]);
+  }, [props.match.params.id]);
 
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  
+  const saveMovie = () => {
+  const addToSavedList = props.addToSavedList;
+  addToSavedList(movie);
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -33,7 +33,7 @@ const Movie = ({match}) => {
   return (
     <div className="save-wrapper">
       <MovieCard key={movie.id} movie={movie} />
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={() => saveMovie()}>Save</div>
     </div>
   );
 };
